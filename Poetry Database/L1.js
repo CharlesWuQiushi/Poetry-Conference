332ac7cfcde0d89c4ae1b28eab2605aa99d14c8d
Charles Wu <35140448+CharlesWuQiushi@users.noreply.github.com>
2019-04-21 17:52:08 +0800

    Create L1.js

---
+++ Poetry Database/L1.js
@@ -0,0 +1,680 @@
+//记录①级题库内容
+var PDL1 = [
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "草一月行飞行二天白鹭上莺清苍长沧",
+        answer: "草长莺飞二月天",
+        from: "高鼎《村居》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "须细归风春雨不斜日",
+        answer: "斜风细雨不须归",
+        from: "张志和《渔歌子》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "敢话言平文生语不轻",
+        answer: "平生不敢轻言语",
+        from: "唐寅《画鸡》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "间识白清要人在风留",
+        answer: "要留清白在人间",
+        from: "于谦《石灰吟》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "四川海东明上山百到",
+        answer: "百川东到海",
+        from: "《长歌行》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "花红于叶月树一霜二",
+        answer: "霜叶红于二月花",
+        from: "杜牧《山行》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "二一三岁去枯荣烟一",
+        answer: "一岁一枯荣",
+        from: "白居易《赋得古原草送别》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "潜鼓随入风吹近箫夜",
+        answer: "随风潜入夜",
+        from: "杜甫《春夜喜雨》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "白媪家谁金黄鸡翁发",
+        answer: "白发谁家翁媪",
+        from: "辛弃疾《清平乐·村居》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "迟香山白风江依尽日",
+        answer: "白日依山尽",
+        from: "王之涣《登鹳雀楼》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "望高身此层只山烟在最申缘",
+        answer: "只缘身在最高层",
+        from: "王安石《登飞来峰》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "柳明稍月望下举上头",
+        answer: "举头望明月",
+        from: "李白《静夜思》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "王送岿又花去不孙去",
+        answer: "又送王孙去",
+        from: "白居易《赋得古原草送别》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "海月残日省朋夜上生",
+        answer: "海日生残夜",
+        from: "王湾《次北固山下》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "海日明知残生升月上",
+        answer: "海上生明月",
+        from: "张九龄《望月怀远》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "战回潮空马古人征背几来城",
+        answer: "古来征战几人回",
+        from: "王翰《凉州词》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "野树绿村天头旷合低",
+        answer: "野旷天低树",
+        from: "孟浩然《宿建德江》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "关度春河在雎鸠不门关之玉窈风州度",
+        answer: "春风不度玉门关",
+        from: "王之涣《凉州词》",
+        used: false
+    },
+    {
+        type: blank,
+        note: "",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "小识认日时不知丽月",
+        answer: "小时不识月",
+        from: "李白《古朗月行》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，space；溯游从之，宛在水中坻。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "溯洄从之 道阻且跻",
+        from: "《诗经·蒹葭》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，长使英雄泪满襟。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "出师未捷身先死",
+        from: "杜甫《蜀相》",
+        used: false
+    },
+    {
+        type: space,
+        note: "蓬头稚子学垂纶，space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "侧坐莓苔草映身",
+        from: " 胡令能《小儿垂钓》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，晴翠接荒城。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "远芳侵古道",
+        from: "白居易《赋得古原草送别》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，万里尚为邻。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "相知无远近",
+        from: "张九龄《送韦城李少府》",
+        used: false
+    },
+    {
+        type: space,
+        note: "海内存知己，space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "天涯若比邻",
+        from: "王勃《送杜少府之任蜀州》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，可爱深红爱浅红？",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "桃花一簇开无主",
+        from: "杜甫《江畔独步寻花·其五》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，平明送客楚山孤。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "寒雨连江夜入吴",
+        from: "王昌龄《芙蓉楼送辛渐》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，space。树木丛生，百草丰茂。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "水何澹澹 山岛竦峙",
+        from: "曹操《观沧海》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，夜吟应觉月光寒。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "晓镜但愁云鬓改",
+        from: "李商隐《无题·相见时难别亦难》",
+        used: false
+    },
+    {
+        type: space,
+        note: "请根据以下线索，说出一个诗人：<br/>一，他是唐朝人；二，他在成都住了四年；<br/>三，号称“少陵野老”。这个诗人是space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "杜甫",
+        from: "",
+        used: false
+    },
+    {
+        type: space,
+        note: "浩荡离愁白日斜，space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "吟鞭东指即天涯",
+        from: "龚自珍《己亥杂诗·其五》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "下列诗句无误的是？",
+        option: {
+            A: "白云深处有人家。",
+            B: "白云生处有人家。",
+            C: "白云升处有人家。",
+        },
+        blankChar: "",
+        answer: B,
+        from: "杜牧《山行》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "《天净沙·秋思》是一首？",
+        option: {
+            A: "小令",
+            B: "词",
+            C: "曲",
+        },
+        blankChar: "",
+        answer: C,
+        used: false
+    },
+    {
+        type: choose,
+        note: "杜牧的《题乌江亭》和李清照的《夏日绝句》都是为哪个人有感而发？",
+        option: {
+            A: "刘邦",
+            B: "项羽",
+            C: "文天祥",
+            D: "岳飞"
+        },
+        blankChar: "",
+        answer: B,
+        from: "杜牧《题乌江亭》；李清照《夏日绝句》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“停车坐爱枫林晚”中，“坐”的意思是？",
+        option: {
+            A: "坐下",
+            B: "座位",
+            C: "因为",
+        },
+        blankChar: "",
+        answer: C,
+        from: "杜牧《山行》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“无为在岐路，儿女共沾巾”中哪一个字是错误的？",
+        option: {
+            A: "无➡少",
+            B: "岐➡歧",
+            C: "巾➡衣",
+        },
+        blankChar: "",
+        answer: B,
+        from: "王勃《送杜少府之任蜀州》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“却看妻子愁何在，慢卷诗书喜欲狂”中哪一个是错误的？",
+        option: {
+            A: "却➡又",
+            B: "慢➡漫",
+            C: "欲➡似",
+        },
+        blankChar: "",
+        answer: B,
+        from: "杜甫《闻官军收河南河北》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“孤帆远影碧空尽，唯见长江天际流”使用了什么修辞手法？",
+        option: {
+            A: "比喻",
+            B: "夸张",
+            C: "借代",
+        },
+        blankChar: "",
+        answer: C,
+        from: "李白《黄鹤楼送孟浩然之广陵》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“但愿人长久，千里共婵娟”中，“婵娟”具体指什么？",
+        option: {
+            A: "月亮",
+            B: "妻子",
+            C: "兄弟",
+        },
+        blankChar: "",
+        answer: A,
+        from: "苏轼《水调歌头》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“忽如一夜春风来，千树万树梨花开”是写的什么季节？",
+        option: {
+            A: "春天",
+            B: "夏天",
+            C: "A、B都对",
+            D: "以上都不对"
+        },
+        blankChar: "",
+        answer: D,
+        from: "岑参《白雪歌送武判官归京》",
+        used: false
+    },
+    {
+        type: space,
+        note: "请根据以下线索，说出一个名胜：<br/>一，白居易写过他附近的鸟；二，杨万里写过他中间的花；<br/>三，苏轼将他比喻为“古代大美女”。<br/>这是space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "西湖",
+        from: "白居易《钱塘湖春行》；杨万里《晓出净慈寺送林子方》；苏轼《饮湖上初晴后雨·其二》",
+        used: false
+    },
+    {
+        type: space,
+        note: "请根据以下线索，说出一句名词：<br/>一，这句名词出自一首宋词；二，词写于中秋；<br/>三，是作者写给他弟弟苏辙的；<br/>四，此联名句借月亮表达了与亲人长相厮守的愿望。<br/>这是space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "但愿人长久，千里共婵娟。",
+        from: "苏轼《水调歌头·明月几时有》",
+        used: false
+    },
+    {
+        type: space,
+        note: "请根据以下线索，说出一首名诗：<br/>一，这首诗共18个字；二，诗的后两句用了3个表颜色的字；<br/>三，诗的前三句是一个相同的字；<br/>四，这首诗是作者7岁是创作的。<br/>这首诗是《space》。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "咏鹅",
+        from: "《咏鹅》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，莲叶何田田。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "江南可采莲",
+        from: "《江南》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，能开二月花。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "解落三秋叶",
+        from: "《江南》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“不闻爷娘唤女声，但闻黄河流水鸣溅溅”中，“溅溅”应该读？",
+        option: {
+            A: "jiàn",
+            B: "jiū",
+            C: "jiān",
+        },
+        blankChar: "",
+        answer: C,
+        from: "《木兰诗》",
+        used: false
+    },
+    {
+        type: choose,
+        note: "“军书十二卷，卷卷有爷名”中，“爷”的意思是？下文中哪个词与它的意思相同？",
+        option: {
+            A: "爷爷（父亲的父亲）；爷娘",
+            B: "父亲；阿爷",
+            C: "父亲；爷娘",
+        },
+        blankChar: "",
+        answer: B,
+        from: "《木兰诗》",
+        used: false
+    },
+    {
+        type: space,
+        note: "春城无处不飞花，space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "寒食东风御柳斜",
+        from: "韩翃《寒食》",
+        used: false
+    },
+    {
+        type: space,
+        note: "九州space，万马space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "生气恃风雷 齐喑究可哀",
+        from: "龚自珍《己亥杂诗·其二百二十》",
+        used: false
+    },
+    {
+        type: space,
+        note: "梅子黄时日日晴，space。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "小溪泛尽却山行",
+        from: "曾几《三衢道中》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，难于上青天。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "蜀道之难",
+        from: "李白《蜀道难》",
+        used: false
+    },
+    {
+        type: space,
+        note: "space，难于上青天。",
+        option: {
+            A: "",
+            B: "",
+            C: "",
+        },
+        blankChar: "",
+        answer: "蜀道之难",
+        from: "李白《蜀道难》",
+        used: false
+    },
+]
